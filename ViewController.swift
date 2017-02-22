09d06c351b4811832c3afa5eaf87c20b1f5b74dc
malibu163 <sdavid@ramapo.edu>
2017-02-21 22:48:08 -0500

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BloodNote
+//
+//  Created by Steve David on 2/21/17.
+//  Copyright © 2017 Steve David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
